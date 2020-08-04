741077e3206267f02721552ea8f4b4d9fa942966
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-08-03 21:31:11 -0300

    Projeto inicial

---
+++ src/main/java/com/registerstudent/RegisterStudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.registerstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterStudentApiApplication.class, args);
+	}
+
+}
